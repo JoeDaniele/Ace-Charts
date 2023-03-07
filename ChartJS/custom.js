0e0e4b03bcf1f37ece9b5aa30bedfd4632ea4a9a
PureYorick <joefdaniele@gmail.com>
2023-03-07 15:02:55 -0600

    New repo
    Moved from Uni to here

---
+++ ChartJS/custom.js
@@ -0,0 +1,70 @@
+var xmlhttp = new XMLHttpRequest();
+var url =
+  'https://water.usace.army.mil/a2w/CWMS_CRREL.cwms_data_api.get_dam_summary_json?p_location_id=2063051&p_unit_system=EN&p_format=JSON';
+const ctx = document.getElementById('canvas');
+xmlhttp.open('GET', url, true);
+xmlhttp.send();
+
+//perhaps we create the maps for each location here and call them
+//in the function below
+xmlhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    var data = JSON.parse(this.responseText);
+    //console.log(data); logs the whole api
+
+    var daily = data[0]['data'][1][1];
+    console.log(daily); //logs todays date e.g. 07-Mar-2023 19:00 GMT
+    //Gotta splice '-2023 19:00 GMT' somehow
+
+    /**
+     * imo we can map out the x-axis with a 30 day span here
+     * once that map is created, within this if statement,
+     * the var time or daily will represent the entire x-axis
+     *
+     * line 33 'labels' will be set to 'labels: time' and gg
+     */
+
+    //  var time = daily.map(function (elem) {
+    //    return elem.time;
+    //  });
+    //  console.log(time);
+
+    var lakeElevation = data[0]['data'][0][1];
+    console.log(lakeElevation); //logs the lake elevation
+  }
+
+  new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+      datasets: [
+        {
+          label: 'Lake Elevation', //'elev' in the api
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: 'transparent',
+          borderColor: 'blue',
+          borderWidth: 4,
+        },
+        {
+          label: 'Conservation Pool',
+          data: [3, 3, 3, 3, 3, 3, 3],
+          backgroundColor: 'transparent',
+          borderColor: 'yellow',
+          borderWidth: 2,
+        },
+      ],
+    },
+    options: {
+      elements: {
+        line: {
+          tension: 0, //less tension == smoother curve -- not accessed properly atm
+        },
+      },
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+};
