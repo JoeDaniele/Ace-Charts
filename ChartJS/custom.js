10db501f5b18ddaf0fff9d8dc39dea575e9db79a
PureYorick <joefdaniele@gmail.com>
2023-03-08 11:00:16 -0600

    Update custom.js
    Changed the url and params in postman to display 30 days of activity instead of 1

--- ChartJS/custom.js
+++ ChartJS/custom.js
@@ -1,6 +1,6 @@
 var xmlhttp = new XMLHttpRequest();
 var url =
-  'https://water.usace.army.mil/a2w/CWMS_CRREL.cwms_data_api.get_dam_summary_json?p_location_id=2063051&p_unit_system=EN&p_format=JSON';
+  'https://water.usace.army.mil/a2w/CWMS_CRREL.cwms_data_api.get_report_json?p_location_id=2063051&p_parameter_type=Flow%3AStor%3APrecip%3AStage%3AElev&p_last=30&p_last_unit=days&p_unit_system=EN&p_format=JSON';
 const ctx = document.getElementById('canvas');
 xmlhttp.open('GET', url, true);
 xmlhttp.send();
@@ -10,27 +10,27 @@ xmlhttp.send();
 xmlhttp.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
     var data = JSON.parse(this.responseText);
-    //console.log(data); logs the whole api
+    console.log(data); // logs the whole api
 
-    var daily = data[0]['data'][1][1];
-    console.log(daily); //logs todays date e.g. 07-Mar-2023 19:00 GMT
-    //Gotta splice '-2023 19:00 GMT' somehow
+    var daily = data[0]['Elev'][0]['time'];
+    console.log('lets find out: ' + daily);
+    //logs todays date e.g. 07-Mar-2023 19:00 GMT
 
     /**
      * imo we can map out the x-axis with a 30 day span here
      * once that map is created, within this if statement,
-     * the var time or daily will represent the entire x-axis
+     * the var 'daily' will represent the entire x-axis
      *
      * line 33 'labels' will be set to 'labels: time' and gg
      */
 
-    //  var time = daily.map(function (elem) {
-    //    return elem.time;
-    //  });
-    //  console.log(time);
+    var time = daily.map(function (elem) {
+      return elem.time;
+    });
+    console.log(time);
 
-    var lakeElevation = data[0]['data'][0][1];
-    console.log(lakeElevation); //logs the lake elevation
+    // var lakeElevation = data[0]['data'][0][1];
+    // console.log(lakeElevation); //logs the lake elevation
   }
 
   new Chart(ctx, {
